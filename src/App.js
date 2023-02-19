cb4e2725f7961e2beba0310258d1ca4e5f1c6b2e
cd-kslji <Kabir.singh@clickdee.com>
2023-02-19 19:37:40 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,35 @@
 import web3 from "./web3";
 import React from "react";
-import {indexerContract} from "./indexer";
+import { indexerContract } from "./indexer";
 
-
-class App extends React.Component{
-  constructor(props){
+class App extends React.Component {
+  constructor(props) {
     super(props);
-    this.state={
-      address:null,
-      proxy:null,
-      amount:0
-    }
+    this.state = {
+      address: null,
+      proxy: null,
+      amount: 0,
+    };
   }
 
-  async componentDidMount(){
-    const accounts=await web3.eth.getAccounts();
+  async componentDidMount() {
+    const accounts = await web3.eth.getAccounts();
     let indexCall = await indexerContract.methods.userLink(accounts[0]).call();
-    let proxy = await indexerContract.methods
-      .accountAddr(indexCall[0])
-      .call();
+    let proxy = await indexerContract.methods.accountAddr(indexCall[0]).call();
     this.setState({
-      address:accounts[0],
-      proxy:proxy
-    })
+      address: accounts[0],
+      proxy: proxy,
+    });
   }
 
-  handleAmountChange=(e)=>{
+  handleAmountChange = (e) => {
     this.setState({
-      amount:e.target.value
-    })
-  }
+      amount: e.target.value,
+    });
+  };
 
-  async handleDeposit(amount,address,proxy){
-    const abi=[
+  async handleDeposit(amount, address, proxy) {
+    const abi = [
       {
         inputs: [
           { internalType: "address", name: "token", type: "address" },
@@ -79,64 +76,265 @@ class App extends React.Component{
       },
     ];
 
-    amount=amount*10**18;
-    
-    const spellData = await web3.eth.abi.encodeFunctionCall(abi[0], [
+    amount = amount * 10 ** 18;
+
+    const spellData = web3.eth.abi.encodeFunctionCall(abi[0], [
       "0x75Ab5AB1Eef154C0352Fc31D2428Cef80C7F8B33",
       amount.toString(),
       "0",
       "0",
     ]);
 
-    const castData = await web3.eth.abi.encodeFunctionCall(abi[1], [
+    const castData = web3.eth.abi.encodeFunctionCall(abi[1], [
       ["AaveV2-v1.2"],
       [spellData],
       address,
     ]);
 
-    const nonce=await web3.eth.getTransactionCount(address,"latest");
+    const nonce = await web3.eth.getTransactionCount(address, "latest");
 
-    let txn={
-      from:address,
+    let txn = {
+      from: address,
       to: proxy,
-    gasLimit: "500000",
-    maxPriorityFeePerGas: "250000000",
-    maxFeePerGas: "5018653759",
-    type: "0x2",
-    nonce: nonce.toString(),
-    data: castData,
-    chainId: 5,
-    }
-this.setState({message:"transaction going through..."})
-    const txHash = await window.ethereum
-    .request({
-        method: 'eth_sendTransaction',
-        params: [txn],
+      gasLimit: "500000",
+      maxPriorityFeePerGas: "250000000",
+      maxFeePerGas: "5018653759",
+      type: "0x2",
+      nonce: nonce.toString(),
+      data: castData,
+      chainId: 5,
+    };
+    this.setState({ message: "transaction going through..." });
+    const txHash = await window.ethereum.request({
+      method: "eth_sendTransaction",
+      params: [txn],
     });
 
     this.setState({
-      message:`Succesfull Transaction! Transaction Hash :${txHash}! Party is pending😉`
-    })
-
-
-
-
-
-
+      message: `Succesfull Transaction! Transaction Hash :${txHash}!!! Party 😉`,
+    });
   }
 
-  render(){
-    return(
+  async handleWithdraw(amount, address, proxy) {
+    const abi = [
+      {
+        inputs: [
+          { internalType: "address", name: "token", type: "address" },
+          { internalType: "uint256", name: "amt", type: "uint256" },
+          { internalType: "uint256", name: "getId", type: "uint256" },
+          { internalType: "uint256", name: "setId", type: "uint256" },
+        ],
+        name: "withdraw",
+        outputs: [
+          { internalType: "string", name: "_eventName", type: "string" },
+          { internalType: "bytes", name: "_eventParam", type: "bytes" },
+        ],
+        stateMutability: "payable",
+        type: "function",
+      },
+      {
+        inputs: [
+          {
+            internalType: "string[]",
+            name: "_targetNames",
+            type: "string[]",
+          },
+          {
+            internalType: "bytes[]",
+            name: "_datas",
+            type: "bytes[]",
+          },
+          {
+            internalType: "address",
+            name: "_origin",
+            type: "address",
+          },
+        ],
+        name: "cast",
+        outputs: [
+          {
+            internalType: "bytes32",
+            name: "",
+            type: "bytes32",
+          },
+        ],
+        stateMutability: "payable",
+        type: "function",
+      },
+    ];
+
+    amount = amount * 10 ** 18;
+
+    const spellData = web3.eth.abi.encodeFunctionCall(abi[0], [
+      "0x75Ab5AB1Eef154C0352Fc31D2428Cef80C7F8B33",
+      amount.toString(),
+      "0",
+      "0",
+    ]);
+
+    const castData = web3.eth.abi.encodeFunctionCall(abi[1], [
+      ["AaveV2-v1.2"],
+      [spellData],
+      address,
+    ]);
+
+    const nonce = await web3.eth.getTransactionCount(address, "latest");
+
+    let txn = {
+      from: address,
+      to: proxy,
+      gasLimit: "500000",
+      maxPriorityFeePerGas: "250000000",
+      maxFeePerGas: "5018653759",
+      type: "0x2",
+      nonce: nonce.toString(),
+      data: castData,
+      chainId: 5,
+    };
+    this.setState({ message: "transaction going through..." });
+    const txHash = await window.ethereum.request({
+      method: "eth_sendTransaction",
+      params: [txn],
+    });
+
+    this.setState({
+      message: `Succesfull Transaction! Transaction Hash :${txHash}!!! Party 😉`,
+    });
+  }
+
+  async handleBorrow(amount, address, proxy) {
+    const abi = [
+      {
+        inputs: [
+          { internalType: "address", name: "token", type: "address" },
+          { internalType: "uint256", name: "amt", type: "uint256" },
+          { internalType: "uint256", name: "rateMode", type: "uint256" },
+          { internalType: "uint256", name: "getId", type: "uint256" },
+          { internalType: "uint256", name: "setId", type: "uint256" },
+        ],
+        name: "borrow",
+        outputs: [
+          { internalType: "string", name: "_eventName", type: "string" },
+          { internalType: "bytes", name: "_eventParam", type: "bytes" },
+        ],
+        stateMutability: "payable",
+        type: "function",
+      },
+      {
+        inputs: [
+          {
+            internalType: "string[]",
+            name: "_targetNames",
+            type: "string[]",
+          },
+          {
+            internalType: "bytes[]",
+            name: "_datas",
+            type: "bytes[]",
+          },
+          {
+            internalType: "address",
+            name: "_origin",
+            type: "address",
+          },
+        ],
+        name: "cast",
+        outputs: [
+          {
+            internalType: "bytes32",
+            name: "",
+            type: "bytes32",
+          },
+        ],
+        stateMutability: "payable",
+        type: "function",
+      },
+    ];
+
+    amount = amount * 10 ** 18;
+
+    const spellData = web3.eth.abi.encodeFunctionCall(abi[0], [
+      "0x75Ab5AB1Eef154C0352Fc31D2428Cef80C7F8B33",
+      amount.toString(),
+      "2",
+      "0",
+      "0",
+    ]);
+
+    const castData = web3.eth.abi.encodeFunctionCall(abi[1], [
+      ["AaveV2-v1.2"],
+      [spellData],
+      address,
+    ]);
+
+    const nonce = await web3.eth.getTransactionCount(address, "latest");
+
+    let txn = {
+      from: address,
+      to: proxy,
+      gasLimit: "500000",
+      maxPriorityFeePerGas: "250000000",
+      maxFeePerGas: "5018653759",
+      type: "0x2",
+      nonce: nonce.toString(),
+      data: castData,
+      chainId: 5,
+    };
+    this.setState({ message: "transaction going through..." });
+    const txHash = await window.ethereum.request({
+      method: "eth_sendTransaction",
+      params: [txn],
+    });
+
+    this.setState({
+      message: `Succesfull Transaction! Transaction Hash :${txHash} 🤟YEAH BOIEE 😎!!!`,
+    });
+  }
+
+  render() {
+    return (
       <div className="App">
         <p>Your Addresss : {this.state.address}</p>
         <p>Your Proxy Address: {this.state.proxy}</p>
-      <h3>1. Deposit</h3>
-      <p>amount</p>
-      <input name="amount" onChange={this.handleAmountChange}/>
-      <button onClick={() => this.handleDeposit(this.state.amount,this.state.address,this.state.proxy)}>Deposit</button>
-      <p>{this.state.message?this.state.message:""}</p>
-    </div>
-    )
+        <h3>1. Aave V2 </h3>
+        <p>amount</p>
+        <input name="amount" onChange={this.handleAmountChange} />
+        <button
+          onClick={() =>
+            this.handleDeposit(
+              this.state.amount,
+              this.state.address,
+              this.state.proxy
+            )
+          }
+        >
+          Deposit
+        </button>
+        <button
+          onClick={() =>
+            this.handleWithdraw(
+              this.state.amount,
+              this.state.address,
+              this.state.proxy
+            )
+          }
+        >
+          Withdraw
+        </button>
+        <button
+          onClick={() =>
+            this.handleBorrow(
+              this.state.amount,
+              this.state.address,
+              this.state.proxy
+            )
+          }
+        >
+          Borrow
+        </button>
+        <p>{this.state.message ? this.state.message : "💃💃💃"}</p>
+      </div>
+    );
   }
 }
 
--- src/indexer.js
+++ src/indexer.js
@@ -1,9 +1,139 @@
 import web3 from "./web3";
 
-const abi=[{"inputs":[{"internalType":"address","name":"_MinimalProxy","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[{"internalType":"uint64","name":"","type":"uint64"}],"name":"accountAddr","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"accountID","outputs":[{"internalType":"uint64","name":"","type":"uint64"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"accounts","outputs":[{"internalType":"uint64","name":"","type":"uint64"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_owner","type":"address"},{"internalType":"uint64","name":"_account","type":"uint64"}],"name":"addAccount","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_account","type":"address"}],"name":"init","outputs":[{"internalType":"uint64","name":"","type":"uint64"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_owner","type":"address"},{"internalType":"uint64","name":"_account","type":"uint64"}],"name":"removeAccount","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"userLink","outputs":[{"internalType":"uint64","name":"first","type":"uint64"},{"internalType":"uint64","name":"last","type":"uint64"},{"internalType":"uint64","name":"count","type":"uint64"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"uint64","name":"","type":"uint64"}],"name":"userList","outputs":[{"internalType":"uint64","name":"prev","type":"uint64"},{"internalType":"uint64","name":"next","type":"uint64"}],"stateMutability":"view","type":"function"}]
+const abi = [
+  {
+    inputs: [
+      { internalType: "address", name: "_MinimalProxy", type: "address" },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [{ internalType: "uint64", name: "", type: "uint64" }],
+    name: "accountAddr",
+    outputs: [{ internalType: "address", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "", type: "address" }],
+    name: "accountID",
+    outputs: [{ internalType: "uint64", name: "", type: "uint64" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "accounts",
+    outputs: [{ internalType: "uint64", name: "", type: "uint64" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "_owner", type: "address" },
+      { internalType: "uint64", name: "_account", type: "uint64" },
+    ],
+    name: "addAccount",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "_account", type: "address" }],
+    name: "init",
+    outputs: [{ internalType: "uint64", name: "", type: "uint64" }],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "_owner", type: "address" },
+      { internalType: "uint64", name: "_account", type: "uint64" },
+    ],
+    name: "removeAccount",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "address", name: "", type: "address" }],
+    name: "userLink",
+    outputs: [
+      { internalType: "uint64", name: "first", type: "uint64" },
+      { internalType: "uint64", name: "last", type: "uint64" },
+      { internalType: "uint64", name: "count", type: "uint64" },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "address", name: "", type: "address" },
+      { internalType: "uint64", name: "", type: "uint64" },
+    ],
+    name: "userList",
+    outputs: [
+      { internalType: "uint64", name: "prev", type: "uint64" },
+      { internalType: "uint64", name: "next", type: "uint64" },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
 
-const indexerAddress="0x5627789041bA487617Eef286657E0343E754081E";
+const indexerAddress = "0x5627789041bA487617Eef286657E0343E754081E";
 
-const indexerContract=new web3.eth.Contract(abi,indexerAddress);
+const indexerContract = new web3.eth.Contract(abi, indexerAddress);
 
-export{indexerContract};
+const buildAbi = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_user",
+        type: "address",
+      },
+    ],
+    name: "build",
+    outputs: [
+      {
+        internalType: "address",
+        name: "_account",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_contract",
+        type: "address",
+      },
+    ],
+    name: "setCloneContract",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_indexContract",
+        type: "address",
+      },
+    ],
+    name: "setindexContract",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+let contractAddress = "0x151c433814e0ED1C4130f6aAD41271805d273Cbe";
+let buildInstance = new web3.eth.Contract(buildAbi, contractAddress);
+
+export { indexerContract, buildInstance };
